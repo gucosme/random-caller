b0bc02e43e117a4cf70d98438072be38a62e5009
Gustavo Cosme <gustavo.cosme@teravoz.com.br>
2019-10-03 13:47:28 -0300

    Add route to get Google SpreadSheet content

--- src/api/router.js
+++ src/api/router.js
@@ -1,10 +1,38 @@
 const { Router } = require('express');
 const VError = require('verror');
+const fetch = require('node-fetch');
+
+const logger = require('../infra/logger');
 
 const router = Router();
 
 router.get('/health', (req, res) => {
   res.json({ status: 'alive' });
+});
+
+router.get('/file/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const response = await fetch(`https://docs.google.com/spreadsheets/d/e/${id}/pub?output=csv`);
+    const buffer = await response.buffer();
+    const content = buffer.toString();
+
+    const [head, ...lines] = content.split('\r\n').map((line) => line.split(','));
+    const entries = lines.map((line) => line.map((item, index) => [head[index], item]));
+
+    const participants = entries.map((entry) => Object.assign(
+      {},
+      ...Array.from(entry, ([k, v]) => ({ [k]: v })),
+    ));
+
+    res.json({ id, participants });
+  } catch (e) {
+    const error = new VError(e, 'Failed to get participants list');
+    logger.error(error);
+
+    next(error);
+  }
 });
 
 router.all('*', (req, res, next) => {
